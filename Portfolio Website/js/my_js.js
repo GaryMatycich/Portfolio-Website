af2c1c7b6b178ff23d93d9beff43f0c0a8f4d094
GaryMatycich <garymatycich@gmail.com>
2021-04-18 20:42:44 -0600

    added videos

---
+++ Portfolio Website/js/my_js.js
@@ -0,0 +1,17 @@
+function openThreeDVid() {
+    document.getElementById("FXVid").style.display = "none";
+    document.getElementById("ThreeDVid").style.display = "block";
+}
+
+function closeThreeDVid() {
+    document.getElementById("ThreeDVid").style.display = "none";
+}
+
+function openFXVid() {
+    document.getElementById("ThreeDVid").style.display = "none";
+    document.getElementById("FXVid").style.display = "block";
+}
+
+function closeFXVid() {
+    document.getElementById("FXVid").style.display = "none";
+}
